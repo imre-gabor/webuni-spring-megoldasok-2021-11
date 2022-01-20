a06a66138211323119e4a66fe38e07a0c711f5b6
imre-gabor <gabor@aut.bme.hu>
2022-01-20 11:24:03 +0100

    use embedded H2 for integration tests

--- src/test/java/hu/webuni/hr/minta/web/EmployeeControllerIT.java
+++ src/test/java/hu/webuni/hr/minta/web/EmployeeControllerIT.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -17,6 +18,7 @@ import org.springframework.test.web.reactive.server.WebTestClient.ResponseSpec;
 import hu.webuni.hr.minta.dto.EmployeeDto;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase
 public class EmployeeControllerIT {
 
 	private static final String BASE_URI = "/api/employees";
