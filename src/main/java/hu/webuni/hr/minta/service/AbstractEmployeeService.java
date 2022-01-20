2623879c4bce4021e96af99a352059391f49e8cf
imre-gabor <gabor@aut.bme.hu>
2022-01-20 11:28:18 +0100

    when creating or updating employee, set the (eventually newly created) position by name, and remove the invalid transient company

--- src/main/java/hu/webuni/hr/minta/service/AbstractEmployeeService.java
+++ src/main/java/hu/webuni/hr/minta/service/AbstractEmployeeService.java
@@ -1,16 +1,16 @@
 package hu.webuni.hr.minta.service;
 
-import java.time.LocalDateTime;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import hu.webuni.hr.minta.model.Employee;
+import hu.webuni.hr.minta.model.Position;
 import hu.webuni.hr.minta.repository.EmployeeRepository;
+import hu.webuni.hr.minta.repository.PositionRepository;
 
 @Service
 public abstract class AbstractEmployeeService implements EmployeeService {
@@ -18,15 +18,38 @@ public abstract class AbstractEmployeeService implements EmployeeService {
 	@Autowired
 	private EmployeeRepository employeeRepository;
 
+	@Autowired
+	private PositionRepository positionRepository;
+
 	@Override
+	@Transactional
 	public Employee save(Employee employee) {
+		clearCompanyAndSetPosition(employee);
 		return employeeRepository.save(employee);
 	}
+	
+	private void clearCompanyAndSetPosition(Employee employee) {
+		employee.setCompany(null);
+		Position position = null;
+		String positionName = employee.getPosition().getName();
+		if(positionName != null) {
+			List<Position> positions = positionRepository.findByName(positionName);
+			if(positions.isEmpty()) {
+				position = positionRepository.save(new Position(positionName, null));
+			} else {
+				position = positions.get(0);
+			}
+		}
+		employee.setPosition(position);
+	}
+
 
 	@Override
 	public Employee update(Employee employee) {
 		if(!employeeRepository.existsById(employee.getEmployeeId()))
 			return null;
+		
+		clearCompanyAndSetPosition(employee);
 		return employeeRepository.save(employee);
 	}
 
